d7ef2f67c2550d2760203347854caa3a239a62da
Ryan Smith <r@32k.io>
2013-09-12 08:53:07 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,17 @@
 $stdout.sync = true
-loop do
-  sleep(1)
-  $stdout.puts("hello world. time=#{Time.now}")
+require "socket"
+
+Thread.new.do
+  loop do
+    sleep(1)
+    $stdout.puts("hello world. time=#{Time.now}")
+  end
+end
+
+ 
+webserver = TCPServer.new('localhost', Integer(ENV['PORT'] || 8000))
+while (session = webserver.accept)
+  session.write(Time.now)
+  session.print("Hello World!")
+  session.close
 end
